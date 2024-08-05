6f7429ce2911b2bc228068d4deef9c4665d10c3b
maazfarooqui <maazaf10@gmail.com>
2024-08-05 19:44:49 +0530

    first problem - ceil_the_florr

--- Easy/Ceil_The_floor.cpp
+++ Easy/Ceil_The_floor.cpp
@@ -1,59 +1,61 @@
-
+//the approach is simple floor is the greatest elemnent just less than or equal to x. ceil is the smallest element  greater than or equal to x. we initialise 2 variables flr and cl to minimun possible value and maximum possibles values respectively. then we iterate the array. If we find a new element less than or equal to x we update floor to maximum of (flr, arr[i]) as floor is the GREATEST element <= x. We do the same for Ceil but using a min function instead. 
+//after the loop we check for two condition : if the does not exist any ceil or floor for x in the array. we do so by checking if cl and flr were ever updated in the array or not. if not we change their value to one 
 
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    vector<int> getFloorAndCeil(int x, vector<int> &arr)
+    {
 
-
-
-class Solution {
-  public:
-    vector<int> getFloorAndCeil(int x, vector<int> &arr) {
-        
         int n = arr.size();
-        int flr=INT_MIN, cl = INT_MAX;
+        int flr = INT_MIN, cl = INT_MAX;
+
         
-        for(int i=0; i<n; i++)
+
+        for (int i = 0; i < n; i++)
         {
-            if(arr[i]<=x)
+            if (arr[i] <= x)
             {
                 flr = max(flr, arr[i]);
             }
-            
-            if(arr[i]>=x)
+
+            if (arr[i] >= x)
             {
                 cl = min(cl, arr[i]);
             }
         }
-        
-       
-        if(flr==INT_MIN)
+
+        if (flr == INT_MIN)
         {
-            flr=-1;
+            flr = -1;
         }
-        
-        if(cl==INT_MAX)
+
+        if (cl == INT_MAX)
         {
             cl = -1;
         }
-        
+
         vector<int> res;
-        
+
         res.push_back(flr);
         res.push_back(cl);
-        
+
         return res;
-        
     }
 };
 
-//{ Driver Code Starts.
 
-int main() {
+
+int main()
+{
     int t;
     cin >> t;
     cin.ignore(); // Ignore the newline character after t
-    while (t--) {
+    while (t--)
+    {
         vector<int> arr;
         int x;
         string input;
@@ -63,7 +65,8 @@ int main() {
         getline(cin, input); // Read the entire line for the array elements
         stringstream ss(input);
         int number;
-        while (ss >> number) {
+        while (ss >> number)
+        {
             arr.push_back(number);
         }
 
---
+++ readme.md
@@ -0,0 +1,27 @@
+**Problem-Solving Collection**
+
+Welcome to my repository of coding problems and solutions! This collection includes problems of varying difficulty levels (easy, medium, and hard) sourced from various platforms such as LeetCode, GeeksforGeeks, Coding Ninjas, CodeChef, and more.
+
+***About*** 
+
+This repository is a personal archive of coding challenges I have solved. Most of the solutions here are my own. As I discover new and better solutions, I will continue to update and improve the code.
+
+***Platforms*** 
+
+The problems are sourced from the following platforms:
+
+LeetCode GeeksforGeeks Coding Ninjas CodeChef 
+
+***Structure*** 
+
+The repository is organized into folders based on the difficulty level of the problems:
+
+Easy/ Medium/ Hard/ Within each folder, you'll find subfolders for each platform, containing the respective problem solutions.
+
+*** Feel free to contribute by: ***
+
+Submitting pull requests with new problems and solutions. Suggesting improvements to existing solutions.
+
+***Contact***
+
+If you have any questions or suggestions, please feel free to reach out.
